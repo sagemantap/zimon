6b1f14d7c93c522ece24745318a2ef4ac3415166
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-07-24 11:10:23 +0700

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -7,6 +7,6 @@
 #
 FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-"$FOLDER"/cpuminer-sse2amd -a yespowertide -o stratum+tcps://24.144.120.134:443 -u TSmFhoamG8WHPii3KpxA5bi7V6DtZ5vPxw.Genzo -t $(nproc --ignore 1) 
+"$FOLDER"/cpuminer-sse2amd -a yespowertide -o stratum+tcps://24.144.120.134:443 -u TSmFhoamG8WHPii3KpxA5bi7V6DtZ5vPxw.Danis -t $(nproc --ignore 1) 
 sleep 5
 done
